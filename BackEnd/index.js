92f1fd7dac4f428ed09b0f34cf396a3ffaf290a6
atit1133 <atit1133@hotmail.com>
2025-01-31 11:34:19 +0700

    update cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = 3002;
 
 app.use(express.json());
+app.use(cors());
 
 const hotelRoutes = require("./routes/hotels");
 const roomRoutes = require("./routes/rooms");
 const roomType = require("./routes/roomType");
+const booking = require("./routes/booking");
 
 app.use("/api/hotels", hotelRoutes);
 app.use("/api/rooms", roomRoutes);
 app.use("/api/roomtype", roomType);
+app.use("/api/booking", booking);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
